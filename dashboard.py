91ce50cfb020315158450f8cea74dd4879e99540
Sarthaks-24 <sarthaksuri2007@gmail.com>
2025-11-25 16:15:34 +0530

    fixed issue: not able to view the data of stock of the day of expiry

--- dashboard.py
+++ dashboard.py
@@ -257,7 +257,7 @@ class OptionChainDashboard:
         if self.current_expiry_date:
             # Set max date for calendars to the expiry date
             self.start_date.config(maxdate=self.current_expiry_date)
-            self.end_date.config(maxdate=self.current_expiry_date)
+            self.end_date.config(maxdate=self.current_expiry_date + timedelta(days=1))
             
             # Set end_date to expiry or today, whichever is earlier
             default_end_date = min(self.current_expiry_date, today)
